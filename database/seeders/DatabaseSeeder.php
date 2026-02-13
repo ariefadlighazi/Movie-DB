b477145012ed91b0f77dea962f0cc79b528d3f88
ariefadlighazi <85598344+ariefadlighazi@users.noreply.github.com>
2026-02-13 16:22:40 +0700

    Fixing bugs for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,26 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        // Use create() instead of factory() to avoid the "fake()" function error
+        User::create([
             'name' => 'Aldmic User',
             'email' => 'aldmic',
-            'password' => Hash::make('123abc123'),
+            'password' => Hash::make('123abc123'), // Fixed password to match requirements
         ]);
-        
     }
 }
